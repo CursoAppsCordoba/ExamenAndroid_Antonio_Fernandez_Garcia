494eabc408fb92819ba6cf93f44f363d0f5a9c13
saitoh00 <afernangar@gmail.com>
2017-11-06 09:22:37 +0100

    Initial release

---
+++ app/src/main/java/com/example/t0ny/agendacontactos/ListarDatos.java
@@ -0,0 +1,104 @@
+package com.example.t0ny.agendacontactos;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import java.util.ArrayList;
+
+public class ListarDatos extends AppCompatActivity implements AdapterView.OnItemClickListener, View.OnClickListener {
+    private ListView listView;
+    private ArrayAdapter adapter;
+    private AlertDialog ventana;
+    private Contacto copia;
+    private ArrayList <Contacto> contactos ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_datos);
+        Button btnvolveratras=(Button)findViewById(R.id.botonvueltaatras);
+        btnvolveratras.setOnClickListener(this);
+        Intent intent = getIntent();
+        contactos= (ArrayList<Contacto>) intent.getSerializableExtra("listacontactos");
+        listView=(ListView)findViewById(R.id.listafinal);
+        adapter=new ArrayAdapter<Contacto>(this,android.R.layout.simple_list_item_1,
+                (ArrayList)intent.getSerializableExtra("listacontactos"));
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+        adapter.notifyDataSetChanged();
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        if(ventana==null){
+            ventana=CreateDialog(contactos.get(i));
+        }
+        ventana.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(MainActivity.EDITAR ==requestCode) {
+            if (resultCode == Activity.RESULT_OK) {
+                if(data.hasExtra("editar")){
+                    Contacto nuevo= data.getParcelableExtra("editar");
+                    System.out.println("Tamaño despues :" + contactos.get(0));
+                    contactos.remove(copia);
+                    contactos.add(nuevo);
+                    System.out.println("Tamaño despues :" + contactos.get(0));
+                    adapter=new ArrayAdapter<Contacto>(this,android.R.layout.simple_list_item_1, contactos);
+
+                    listView.setAdapter(adapter);
+
+                    //System.out.println("Tamaño despues :" + contactos.get(0));  //Prueba por A.Monitor para ver si se ha editado el contacto
+
+
+
+                }
+            }
+        }
+    }
+
+    public AlertDialog CreateDialog(final Contacto c) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Estás seguro de quieres editar este contacto?");
+        builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                Intent intent;
+                intent=new Intent(ListarDatos.this, EditarDatos.class);
+                copia= c;
+                intent.putExtra("contacto", c);
+                startActivityForResult(intent, MainActivity.EDITAR);
+            }
+        });
+
+        builder.setNegativeButton("cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+
+            }
+        });
+        return builder.create();
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent inte = new Intent(this, MainActivity.class);
+        inte.putExtra("modificado", contactos);
+        startActivity(inte);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/t0ny/agendacontactos/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.t0ny.agendacontactos;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final Integer ALTA=100;
+    public static final Integer BAJA=200;
+    public static final Integer LISTAR=300;
+    public static final Integer EDITAR=400;
+    public ArrayList<Contacto> listaContactos = new ArrayList();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnalta=(Button)findViewById(R.id.botonadd);
+        btnalta.setOnClickListener(this);
+        Button btnborrar=(Button)findViewById(R.id.botonborrar);
+        btnborrar.setOnClickListener(this);
+        Button btnlistar=(Button)findViewById(R.id.botonvertodos);
+        btnlistar.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (ALTA == requestCode) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (data.hasExtra("alta")) {
+                 //pruebas  System.out.println("Tamaño antes :" + listaContactos.size());
+                    listaContactos.add((Contacto) data.getParcelableExtra("alta"));  //se añade contacto que previamente hemos pasado con un Intent desde la actividad GrabarDatos
+                 //pruebas   System.out.println("Tamaño despues :" + listaContactos.size());
+                  // pruebas  System.out.println("Tamaño despues :" + listaContactos.get(0));
+                }
+            }
+        } else {
+            if (BAJA == requestCode) {
+                if (resultCode == Activity.RESULT_OK) {
+                    if (data.hasExtra("borrar")) {
+                        // System.out.println("Tamaño antes :" + listaContactos.size());  //verificacion del tamaño para saber si se hace el borrado/
+                        Contacto contacto = data.getParcelableExtra("borrar");  //se borra contacto que previamente hemos pasado con un Intent desde la actividad BorrarDatos
+                        //  System.out.println("Tamaño despues :" + listaContactos.size());
+                        Toast.makeText(this, listaContactos.remove(contacto) ? "Se ha borrado un contacto" : "No existe un contacto con esos datos", Toast.LENGTH_LONG).show();
+
+                    }
+                }
+            } else {
+                if (LISTAR == requestCode) {
+                    if (resultCode == Activity.RESULT_OK) {
+                        if (data.hasExtra("modificado")){
+                            listaContactos= data.getParcelableArrayListExtra("modificado");
+                        }
+
+                    }
+
+                }
+            }
+        }
+    }
+
+
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (getIntent().hasExtra("modificado")){
+            listaContactos = getIntent().getParcelableArrayListExtra("modificado");
+        }
+    }
+
+    @Override
+        public void onClick(View v) {
+        Intent intent;
+        switch(v.getId()){
+            case R.id.botonadd:
+                intent=new Intent(this, GrabarDatos.class);
+                startActivityForResult(intent, ALTA);  //mandamos un intent junto con un Integer para definin la Actividad a la que llamamos
+                break;
+            case R.id.botonborrar:
+                intent=new Intent(this, BorrarDatos.class);
+                startActivityForResult(intent, BAJA);
+                break;
+            case R.id.botonvertodos:
+                intent=new Intent(this, ListarDatos.class);
+                intent.putExtra("listacontactos", listaContactos);
+                startActivity(intent);
+                break;
+        }
+    }
+}
